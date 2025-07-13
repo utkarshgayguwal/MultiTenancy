10f8aea022aefd9d8b4019552dfc1d65f3f6f712
Utkarsh Gayguwal <utkarshgayguwal123@gmail.com>
2025-07-13 20:39:23 +0000

    Filter Model By user_id: With Global Scope in booted function. (note: it only works with Eloquent queries and not works with raw queries)

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model; 
 use Illuminate\Database\Eloquent\Observers\ObservedBy; 
 use App\Observers\ProjectObserver; 
+use Illuminate\Database\Eloquent\Builder;
 
-#[ObservedBy(ProjectObserver::class)]
+// #[ObservedBy(ProjectObserver::class)]
 class Project extends Model
 {
     protected $guarded = ['id'];
@@ -15,10 +16,14 @@ class Project extends Model
         return $this->belongsTo(User::class);
     }
 
-    // protected static function booted()
-    // {
-    //     static::creating(function (Project $project) {
-    //         $project->user_id = auth()->id();
-    //     });
-    // }
+    protected static function booted()
+    {
+        static::creating(function (Project $project) {
+            $project->user_id = auth()->id();
+        });
+
+        static::addGlobalScope(function (Builder $builder){
+            $builder->where('user_id', auth()->id());
+        });
+    }
 }
